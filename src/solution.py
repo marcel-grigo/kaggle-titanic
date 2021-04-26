b7924125c3f62fea23e5a5597ff07808ef908e9d
Marcel Grigo <rddlr.work@gmail.com>
2021-04-26 18:10:49 +0200

    added the python file for solution

---
+++ src/solution.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 26 17:54:10 2021
+
+@author: marcel grigo
+"""
+
+import pandas as pd
+df_test=pd.read_csv('../data/test.csv', delimiter=',')
+df_train=pd.read_csv('../data/train.csv', delimiter=',')
+df_gt=pd.read_csv('../data/gender_submission.csv', delimiter=',')
+
